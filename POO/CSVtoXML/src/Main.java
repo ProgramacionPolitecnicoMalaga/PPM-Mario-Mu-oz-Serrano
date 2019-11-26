20a15755dd69655f5715f7d5f969839df70fb003
Mario Muñoz <mario.ms90@gmail.com>
2019-11-26 10:27:55 +0100

    fix

--- POO/CSVtoXML/src/Main.java
+++ POO/CSVtoXML/src/Main.java
@@ -1,13 +1,13 @@
-public class Main {
+﻿public class Main {
 
     public static void main(String[] args) {
         Conversor conversor = new Conversor();
-        System.out.print("El conversor con cadena String tardó en completar la tarea ");
+        System.out.print("El conversor con cadena completó la tarea en ");
         long inicioConversorCadena = System.nanoTime();
         conversor.convertirConCadena();
         System.out.println((System.nanoTime() - inicioConversorCadena)/1000000 + " milisegundos");
         System.out.println("---------------------------------");
-        System.out.print("El conversor con nodos Dom tardó en completar la tarea ");
+        System.out.print("El conversor con nodos dom completó la tarea en ");
         long inicioConversorConNodos = System.nanoTime();
         conversor.convertirConNodos();
         System.out.println((System.nanoTime() - inicioConversorConNodos)/1000000 + " milisegundos");
